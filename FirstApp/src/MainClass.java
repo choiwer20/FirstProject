0e66a3d4fd660fc9c1da4d880b4483161f021342
choiwer20 <choiwer20@naver.com>
2014-04-21 10:23:03 +0900

    main클래스 작업()

---
+++ FirstApp/src/MainClass.java
@@ -0,0 +1,17 @@
+
+public class MainClass {
+	public MainClass() {
+	// TODO Auto-generated constructor stub
+	}
+	
+	//master라고 부름
+	public static void main(String[] args) {
+		//branch..
+			System.out.println("첫번째 프로젝트");
+			
+				for(int i=0;i<5;i++) {
+					System.out.println(""+i);
+				}			
+	}
+	
+}
